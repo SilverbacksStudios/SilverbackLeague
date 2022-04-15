543c921a83df7518ef5bbad1126a1ba5204b21f0
Joakim Malmberg <10110847+JockeM@users.noreply.github.com>
2022-04-15 11:46:37 +0200

    big prgram cahnges :)

--- logic.js
+++ logic.js
@@ -1,25 +1,34 @@
-const Express = require("express");
-const res = require("express/lib/response");
-const path= require( "path");
-const { allowedNodeEnvironmentFlags } = require("process");
+const express = require("express");
+const path = require("path");
 
-const app = Express()
+const app = express()
 const port = 3000;
 
-var Markus = 1;
-var Martin = 0;
-// GET, PUT, POST, DELETE
+const scores = {
+  markus: 0,
+  martin: 1,
+  jocke: 1337,
+};
 
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '/index.html'));
-  });
-  
-  app.get('/score',function(req,res){
-      res.send({"M":Markus,"MM":Martin});
-        })
-app.post("/addMarkus", function(req,res){
-    Markus++;
-    res.sendStatus(200);
+app.get("/scores", function (_, res) {
+  res.status(200).send(scores);
 })
+
+app.post("/scores/add", function (req, res) {
+  if (req.query.player === undefined) {
+    return res.status(400).send("No player name given");
+  }
+
+  const score = scores[req.query.player];
+  if (score === undefined) {
+    scores[req.query.player] = 0;
+  } else {
+    scores[req.query.player]++;
+  }
+
+  res.status(200).send(scores);
+})
+
 app.listen(port, () => console.log("listening on port" + port))

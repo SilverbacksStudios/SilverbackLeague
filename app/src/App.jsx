fd851e6e48ce182a9cd45d8da846af4fe9960bff
LokanderVisiba <Markus.lokander@Visibacare.com>
2022-04-23 16:18:09 +0200

    added react component,doesn't work yet.

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,45 +1,56 @@
-import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
+import { useState, useEffect } from 'react'
+import { supabase } from './supabase'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [players, setPlayers] = useState ([])
+  const [player, setPlayer] = useState ({ name: "", points: ""})
+  const { name, points } = player
+  useEffect(() => {
+    fetchPlayers()
+  }, [])
+  async function fetchPlayers(){
+    const { data } = await supabase
+      .from('players')
+      .select()
+    setPlayers(data)
+    console.log("data: ", data)
+  }
+  async function createPlayer(){
+    await supabase
+     .from('players')
+     .insert([
+        { name, points }
+     ])
+       .single()
+      setPlayer({ name: "", points: ""})
+      fetchPlayers()
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <input
+     placeholder='Name'
+     value={name}
+     onChange = {e => setPlayers({ ...player, name: e.target.value})}
+     />
+     <input
+     placeholder='Points'
+     value={points}
+     onChange={e => setPlayers({ ...player, points: e.target.value})}
+     />
+     <button onClick={createPlayer}>Create Player</button>
+     {  
+       players.map(player => (
+        <div key={players.id}>
+         <h3>{player.name}</h3>
+         <p>{player.points}</p>
+        </div>
+       ))
+     }
     </div>
   )
 }
 
 export default App
+

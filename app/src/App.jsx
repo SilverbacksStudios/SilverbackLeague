c4464b830e96b50d6023ffc38424a5e59e3b7035
LokanderVisiba <Markus.lokander@Visibacare.com>
2022-05-22 14:49:42 +0200

    Auth changes

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,18 +1,35 @@
 import "./App.css";
 import Home from "./pages/Home";
-import Auth from "./pages/Auth";
 import Startpage from "./pages/startPage";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Auth } from "./pages/Auth";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  useNavigate,
+  useLocation,
+  Navigate,
+  Outlet,
+} from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Startpage />} />
-        <Route path="Home" element={<Home />} />
-        <Route path="auth" element={<Auth />} />
-      </Routes>
-    </BrowserRouter>
+    <Authprovider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Startpage />} />
+          <Route
+            path="/Home"
+            element={
+              <RequireAuth>
+                <ProtectedPage />
+              </RequireAuth>
+            }
+          />
+          <Route path="auth" element={<Auth />} />
+        </Routes>
+      </BrowserRouter>
+    </Authprovider>
   );
 }
 
--- app/src/pages/Auth.jsx
+++ app/src/pages/Auth.jsx
@@ -1,45 +1,56 @@
-import React, {useState} from "react"
-import { supabase } from "../Database/supabase"
-import {useNavigate} from 'react-router-dom';
+import "../App.css";
+import React, { useState } from "react";
+import { supabase } from "../Database/supabase";
+import { useNavigate } from "react-router-dom";
 
-export default function Auth(){
-    const navigate = useNavigate();
-    const [email, setEmail]= useState('')
-    const [password, setPassword]= useState('')
+function Auth() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    return(
-        <div>
-         <input placeholder ="Email" value={email} onChange={(e) => setEmail (e.target.value)} />
-         <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword (e.target.value)} />
-         <button onClick={() => loggain(email, password, navigate)}>logga in</button>
-         <button onClick={() => skapakonto(email, password, navigate)}>skapakonto</button>
-        </div>
-    )
+  return (
+    <div>
+      <input
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={() => loggain(email, password, navigate)}>
+        logga in
+      </button>
+      <button onClick={() => skapakonto(email, password, navigate)}>
+        skapakonto
+      </button>
+    </div>
+  );
 }
 
-async function loggain(email, password, navigate){
-    try{
-    const {error} = await supabase.auth.signIn({email, password})
-      if(error) throw error
-      alert('inloggad')
-      useHistory.push('/Home')
-    } 
-    catch(error) {
-        alert(error.message)
-    }
-
-
+async function loggain(email, password, navigate) {
+  try {
+    const { error } = await supabase.auth.signIn({ email, password });
+    if (error) throw error;
+    alert("inloggad");
+    useHistory.push("/Home");
+  } catch (error) {
+    alert(error.message);
+  }
 }
 
 const skapakonto = async (email, password, navigate) => {
-    try{
-    const {error} = await supabase.auth.signUp({email, password})
-      if(error) throw error
-      alert('inloggad')
-      navigate.push('/Home')
-    } 
-    catch(error) {
-        alert(error.message)
-    }
-    
-}
+  try {
+    const { error } = await supabase.auth.signUp({ email, password });
+    if (error) throw error;
+    alert("inloggad");
+    navigate.push("/Home");
+  } catch (error) {
+    alert(error.message);
+  }
+};
+
+export { Auth };
